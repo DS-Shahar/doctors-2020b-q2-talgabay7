72657d611b3907fb5e421d437be49245957eef9c
talgabay7 <126403499+talgabay7@users.noreply.github.com>
2024-03-17 09:07:45 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,20 @@
 class Main {
+  public static void doctorRating(Doctor[] doctors) {
+		double sum=0;
+		int count=0;
+		int r;
+		for(int i=0;i<doctors.length;i++) {
+			System.out.println("enter your rate ");
+			r=input.nextInt();
+			while(r!=-1) {
+				sum+=r;
+				count++;
+				System.out.println("enter your rate ");
+				r=input.nextInt();
+			}
+			doctors[i].setRate(sum/count);
+		}
+	}
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
